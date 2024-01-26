320a527ab1e8ec836f924c9c442e4a6e8967cbce
Stéphane Daviet <stephane.daviet@maif.fr>
2024-01-26 09:55:30 +0100

    Start

---
+++ src/main/java/fr/sample/webjarsdemo/WebjarsDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.sample.webjarsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjarsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjarsDemoApplication.class, args);
+	}
+
+}
